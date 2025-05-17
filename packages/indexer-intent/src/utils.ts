af9c406e3270f2d1f428ab37b162cdd5b8e0823f
seve <ferguspatricio@gmail.com>
2025-05-17 20:30:57 +0700

    delete indexer-intent

--- packages/indexer-intent/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs';
-
-// timestamp to date
-export function readableTimestamp(timestamp: bigint): string {
-  const date = dayjs.unix(Number(timestamp)).format('YYYY-MM-DD HH:mm:ss');
-  return date;
-}
